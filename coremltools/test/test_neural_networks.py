c67d7a6161062d1d2ebaf88e10d204ece62c5c23
Allen Lin <shuoxin_lin@apple.com>
2017-08-21 16:12:15 -0700

    remove redundant changes

--- coremltools/test/test_neural_networks.py
+++ coremltools/test/test_neural_networks.py
@@ -135,22 +135,4 @@ class BasicNumericCorrectnessTest(unittest.TestCase):
         for i in range(0, num_channels2):
             self.assertAlmostEquals(fullOutputs['middle_layer_output'][i], partialOutput['output2'][i], 2)
             
-    def test_undefined_shape_single_output(self):
-        W = np.ones((3,3))
-        input_features = [('data', datatypes.Array(3))]
-        output_features = [('probs', None)]
-        builder = NeuralNetworkBuilder(input_features, output_features)
-        builder.add_inner_product(name = 'ip1', 
-                                  W = W, 
-                                  b = None, 
-                                  input_channels = 3, 
-                                  output_channels = 3,
-                                  has_bias = False, 
-                                  input_name = 'data', 
-                                  output_name = 'probs')
-        mlmodel = MLModel(builder.spec)
-        data = np.ones((3,))
-        data_dict = {'data': data}
-        probs = mlmodel.predict(data_dict)['probs']
-        self.assertTrue(np.allclose(probs, np.ones(3) * 3))
 
