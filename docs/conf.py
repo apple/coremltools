3b4e552adad883e008e40addd6077d94c4e2de3f
Sohaib Qureshi <smq@apple.com>
2017-08-23 15:28:15 -0700

    Updated version number for release

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ import pkg_resources
 try:
     version = pkg_resources.require("coremltools")[0].version
 except:
-    version = "0.5.1"
+    version = "0.6.0"
 
 # The short X.Y version.
 version = version
