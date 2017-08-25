5502f0227ded7cfc82ef3393773c2b9c47891692
Krishna Sridhar <>
2017-08-25 15:19:45 -0700

    Uses the on-device compiler for this.

--- coremlpython/CoreMLPython.mm
+++ coremlpython/CoreMLPython.mm
@@ -1,4 +1,4 @@
-
+#import <CoreML/CoreML.h>
 #import "CoreMLPythonArray.h"
 #import "CoreMLPython.h"
 #import "CoreMLPythonUtils.h"
@@ -34,56 +34,6 @@ py::dict Model::predict(const py::dict& input, bool useCPUOnly) {
     }
 }
 
-static NSString* _cached_compiler_path = nil;
-
-NSString * getCompilerPath()
-{
-    if(_cached_compiler_path != nil) {
-        return _cached_compiler_path;
-    }
-    
-    // For AppleInternal builds
-    NSFileManager *fm = [NSFileManager defaultManager];
-    BOOL isDir = NO;
-    if ([fm fileExistsAtPath:@"/AppleInternal/" isDirectory:&isDir]) {
-        if (isDir) {
-            NSString *internalCompilerPath = @"/usr/local/bin/coremlcompiler";
-            if ([fm fileExistsAtPath:internalCompilerPath]) {
-                return internalCompilerPath;
-            }
-        }
-    }
-    
-    // Otherwise we return the xcode toolchain path
-    NSPipe *pipe = [NSPipe pipe];
-    NSFileHandle *file = pipe.fileHandleForReading;
-    NSTask *task = [[NSTask alloc] init];
-    task.launchPath = @"/usr/bin/xcrun";
-    task.arguments = @[@"-f", @"coremlcompiler"];
-    task.standardError = pipe;
-    task.standardOutput = pipe;
-    [task launch];
-    NSData *data = [file readDataToEndOfFile];
-    [file closeFile];
-    
-    [task waitUntilExit];
-    
-    NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    output = [output stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    
-    if (task.terminationStatus != 0) {
-        std::stringstream errmsg;
-        errmsg << "Got non-zero exit code ";
-        errmsg << task.terminationStatus;
-        errmsg << " from xcrun. Output was: ";
-        errmsg << output.UTF8String;
-        throw std::runtime_error(errmsg.str());
-    }
-    
-    _cached_compiler_path = output;
-    return output;
-}
-
 Model Model::fromSpec(const std::string& urlStr) {
     @autoreleasepool {
         // compile to a temp dir, then load from there
@@ -91,63 +41,34 @@ Model Model::fromSpec(const std::string& urlStr) {
         NSURL *tempDir = [NSURL fileURLWithPath:[NSTemporaryDirectory() stringByAppendingPathComponent:[[NSProcessInfo processInfo] globallyUniqueString]] isDirectory:YES];
         [[NSFileManager defaultManager] createDirectoryAtURL:tempDir withIntermediateDirectories:YES attributes:nil error:&error];
         Utils::handleError(error);
-        
+
         NSURL *specUrl = Utils::stringToNSURL(urlStr);
-        
+
         // Get a temporary directory in which to save the compiled model.
         NSString *tmp_dir = NSTemporaryDirectory();
         NSString *model_name = [[specUrl URLByDeletingPathExtension] lastPathComponent];
         NSString *basename = [tmp_dir stringByAppendingString:[NSString stringWithFormat:@"/%@.mlmodelc", model_name]];
         NSURL *compiledUrl = [NSURL fileURLWithPath:basename isDirectory:TRUE];
-        
+
         NSFileManager *fileManager = [NSFileManager defaultManager];
-        
         if(![fileManager createDirectoryAtPath:tmp_dir withIntermediateDirectories:YES attributes:nil error:&error]) {
                 // An error has occurred, do something to handle it
             NSLog(@"Failed to create directory \"%@\". Error: %@", basename, error);
         }
-        
-        @try {
-            // Get compiler path
-            NSString *output = getCompilerPath();
-            NSPipe *pipe = [NSPipe pipe];
-            NSFileHandle *file = pipe.fileHandleForReading;
-            NSTask *task = [[NSTask alloc] init];
-            task.launchPath = output;
-            task.arguments = @[@"compile", specUrl.path, tmp_dir];
-            task.standardError = pipe;
-            task.standardOutput = pipe;
-            [task launch];
-            NSData *data = [file readDataToEndOfFile];
-            [file closeFile];
-            
-            [task waitUntilExit];
-            output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-            output = [output stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 
-            if (task.terminationStatus != 0) {
-                std::stringstream errmsg;
-                errmsg << "Got non-zero exit code ";
-                errmsg << task.terminationStatus;
-                errmsg << " from coremlcompiler: ";
-                errmsg << output.UTF8String;
-                throw std::runtime_error(errmsg.str());
-            }
-            
-        }
-        @catch (NSException *exception) {
-         
-            // translate into a type that pybind11 can bridge to Python
+        // Compile the model first.
+        error = nil;
+        compiledUrl = [MLModel compileModelAtURL:specUrl error:&error];
+
+        // Translate into a type that pybind11 can bridge to Python
+        if (error != nil) {
             std::stringstream errmsg;
             errmsg << "Encountered exception \"";
-            errmsg << exception.name.UTF8String;
-            errmsg << "\" while attempting to invoke coremlcompiler: \"";
-            errmsg << exception.reason.UTF8String;
+            errmsg << error.localizedDescription.UTF8String;
+            errmsg << "\" while compiling the CoreML model. \"";
             errmsg << "\".";
             throw std::runtime_error(errmsg.str());
-            
         }
-        
         return Model(compiledUrl.fileSystemRepresentation);
     }
 }
