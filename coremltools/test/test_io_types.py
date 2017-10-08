b0c64c157e9bf876e2729c338222d4b1e359bd95
Toby Roseman <troseman@apple.com>
2017-10-07 21:17:15 -0700

    removed iteritems()

--- coremltools/test/test_io_types.py
+++ coremltools/test/test_io_types.py
@@ -218,7 +218,7 @@ class TestIODataTypes(unittest.TestCase):
         model = keras.models.Sequential()
         model.add(keras.layers.Dense(3, activation='sigmoid', kernel_initializer='random_uniform',
                                      bias_initializer='random_uniform', input_dim=3))
-        for key, dtype in self.number_data_type.iteritems():
+        for key, dtype in self.number_data_type.items():
             try:
                 input_data = np.random.rand(1, 3).astype(key)
                 keras_out = model.predict(input_data)
@@ -247,7 +247,7 @@ class TestIODataTypes(unittest.TestCase):
 
         model = keras.models.Sequential()
         model.add(keras.layers.Embedding(100, 3, input_length=5, input_dtype='float32'))
-        for key, dtype in self.number_data_type.iteritems():
+        for key, dtype in self.number_data_type.items():
             try:
                 input_data = np.random.randint(0, 100, size=(1, 5)).astype(key)
                 keras_out = np.squeeze(model.predict(input_data)).flatten()
