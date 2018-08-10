36afbe4678f52154c9014b6ee09668a910cd8ebe
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-08-10 16:51:19 -0700

    Upload only after tests pass, not before testing

--- ci/build.py
+++ ci/build.py
@@ -17,6 +17,9 @@ def main():
         with open('ci/.pypirc') as base_pypirc, (Path.home() / '.pypirc').open('w') as pypirc:
             pypirc.write(base_pypirc.read())
             pypirc.write('password: {}\n'.format(pypi_password))
+        # For some reason coremltools hardcodes the wheel tag to py2.7 in setup.cfg, even in Python 3 installs.
+        # We have to get rid of it, else PyPI and pip will believe the target is Python 2.7 regardless of where it was built.
+        Path('coremltools/setup.cfg').unlink()
         setup('upload')
     else:
         setup()
