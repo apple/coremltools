ccd70dbb230167aa738634acb820170c36c85ceb
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2018-08-13 11:05:30 -0700

    Run CMake build of native deps before CI test

--- ci/test.py
+++ ci/test.py
@@ -5,21 +5,37 @@ import traceback
 
 from custom_venv import CustomVenv
 
+
+def run(*command, **kwargs):
+    subprocess.run(command, cwd='coremltools', check=True, **kwargs)
+
+
 def run_tests():
-    env = CustomVenv(clear=True, symlinks=True, with_pip=True, requirements=['-r', 'ci/test_requirements.txt'])
+    env = CustomVenv(clear=True, symlinks=True, with_pip=True, requirements=['-r', 'coremltools/test_requirements.pip'])
     env.create('venv')
-    subprocess.run([env.python, '-m', 'pytest', 'coremltools'], check=True)
+
+    cmake_environment = os.environ.copy()
+    cmake_environment['CMAKE_BUILD_TYPE'] = 'Release'
+    cmake_environment['PATH'] = os.pathsep.join((os.path.dirname(env.python), cmake_environment['PATH']))
+    cmake_environment['PYTHONPATH'] = os.pathsep.join((os.getcwd(), cmake_environment.get('PYTHONPATH', '')))
+    run('cmake', '.', env=cmake_environment)
+    run('cmake', '--build', '.', '--target', 'pytest', '--config', 'Release', env=cmake_environment)
+
+
+def tests_should_pass():
+    if sys.version_info >= (3, 7) or sys.version_info < (3, 5) and os.name == 'nt':
+        print('Some dependencies are unavailable for this Python version in this system, tests are expected to fail')
+        return False
+    if os.name == 'nt':
+        print('Native compilation is failing in Windows, tests are expected to fail')
+        return False
+    return True
 
 
 def main():
-    if sys.version_info >= (3, 7) or sys.version_info < (3, 5) and os.name == 'nt':
-        print('Some dependencies are unavailable for this Python version in this system, tests are expected to fail')
-        try:
-            run_tests()
-        except Exception:  # Don't fail the CI build
-            traceback.print_exc()
+    if tests_should_pass():
+        run_tests()
     else:
-        print('TODO Find out how to expose pybind11_tests so coremltools tests succeed')
         try:
             run_tests()
         except Exception:  # Don't fail the CI build
