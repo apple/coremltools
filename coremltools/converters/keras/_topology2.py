7cf3c00f275a3c71b7ee0563b25b00d48fe2f4d7
Vladislav Samsonov <vladislav.samsonov@phystech.edu>
2018-09-12 19:38:41 +0300

    Fix missing out activations for SeparableConv2D and SeparableConv1D while defusing

--- coremltools/converters/keras/_topology2.py
+++ coremltools/converters/keras/_topology2.py
@@ -506,6 +506,8 @@ class NetGraph(object):
                 k_layer = k_layer.layer
             if (isinstance(k_layer, _keras.layers.Conv2D) or 
                 isinstance(k_layer, _keras.layers.Conv1D) or 
+                isinstance(k_layer, _keras.layers.SeparableConv2D) or 
+                isinstance(k_layer, _keras.layers.SeparableConv1D) or 
                 isinstance(k_layer, _keras.layers.Dense)):
                 
                 func_name = k_layer.activation.func_name if six.PY2 else \
