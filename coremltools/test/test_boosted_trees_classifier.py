8f4b6aba98498a79506fc4e74c029fd4704efb6b
Zaccharie Ramzi <zaccharie@xbird.io>
2018-11-20 15:19:40 +0100

    corrected dump of classifier

--- coremltools/test/test_boosted_trees_classifier.py
+++ coremltools/test/test_boosted_trees_classifier.py
@@ -272,7 +272,7 @@ class GradientBoostingMulticlassClassifierXGboostTest(unittest.TestCase):
         n_classes = np.unique(self.target)
 
         xgb_model_json = tempfile.mktemp('xgb_tree_model_classifier.json')
-        xgb_json_out = self.xgb_model.get_dump(with_stats=True, dump_format='json')
+        xgb_json_out = self.xgb_model.get_booster().get_dump(with_stats=True, dump_format='json')
         with open(xgb_model_json, 'w') as f:
             json.dump(xgb_json_out, f)
         spec = xgb_converter.convert(
