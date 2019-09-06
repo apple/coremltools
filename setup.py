efb41a0d693d3273bb6f751c14f29a8d4ded54a1
aseemw <aseem.elec@gmail.com>
2019-09-06 13:21:37 -0700

    3.0b6 release

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(README) as f:
     long_description = f.read()
 
 setup(name='coremltools',
-    version='3.0b5',
+    version='3.0b6',
     description='Community Tools for CoreML',
     long_description=long_description,
     author='Apple Inc.',
