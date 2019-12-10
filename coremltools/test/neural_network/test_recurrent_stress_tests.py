e1d005d29ef05b31dde573b564db907d6001245d
bhushans_22 <bsonawane@apple.com>
2019-12-10 10:49:57 +0530

    Reverting LSTM test_initial_state_LSTM test case index

--- coremltools/test/neural_network/test_recurrent_stress_tests.py
+++ coremltools/test/neural_network/test_recurrent_stress_tests.py
@@ -1528,7 +1528,7 @@ class InitialStateRecurrentModels(unittest.TestCase):
             np.testing.assert_array_almost_equal(coreml_output_1.T, keras_output_1)
 
         hidden_state = (np.random.rand(1, 5), np.random.rand(1, 5))
-        model.get_layer(index=0).reset_states(hidden_state)
+        model.get_layer(index=1).reset_states(hidden_state)
 
         coreml_model = keras_converter.convert(model=model, input_names='data', output_names='output')
         spec = coreml_model.get_spec()
