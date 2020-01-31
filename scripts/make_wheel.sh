69b3a757cff558f94e6aae061e0fd7657d0e83f5
Krishna Sridhar <srikrishna_sridhar@apple.com>
2020-01-31 09:26:14 -0800

    Updated the testing and build scripts.

--- scripts/make_wheel.sh
+++ scripts/make_wheel.sh
@@ -45,7 +45,7 @@ echo "Configuring using python from $PYTHON"
 echo
 echo ${COREMLTOOLS_HOME}
 cd ${COREMLTOOLS_HOME}
-bash -e configure --python=$PYTHON
+bash -e configure --python=$PYTHON --build-only
 
 # Setup the right python
 source scripts/python_env.sh
