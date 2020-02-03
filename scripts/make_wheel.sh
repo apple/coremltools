be11797808705dd23463fa0fc10bc9a3aaa7cf89
Krishna Sridhar <srikrishna_sridha@apple.com>
2020-02-03 14:35:04 -0800

    Automated the documentation creation as part of the build scripts.

--- scripts/make_wheel.sh
+++ scripts/make_wheel.sh
@@ -45,7 +45,7 @@ echo "Configuring using python from $PYTHON"
 echo
 echo ${COREMLTOOLS_HOME}
 cd ${COREMLTOOLS_HOME}
-bash -e configure --python=$PYTHON --build-only
+bash -e configure --python=$PYTHON --exclude-test-deps
 
 # Setup the right python
 source scripts/python_env.sh
