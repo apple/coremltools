d8bb83b7b616c0e28ab41d2061ef378e405eb8e2
Zach Nation <znation@apple.com>
2020-07-04 17:07:35 -0700

    Try to fix weird issue in build script
    pip uninstall never should have worked outside of a virtualenv

--- scripts/build.sh
+++ scripts/build.sh
@@ -62,13 +62,14 @@ if [[ $CHECK_ENV == 1 ]]; then
     zsh -i -e scripts/env_create.sh --python=$PYTHON --exclude-test-deps
 fi
 
-pip uninstall -y coremltools
-
 # Setup the right python
 source scripts/env_activate.sh --python=$PYTHON
 echo
 echo "Using python from $(which python)"
 echo
+
+# Uninstall any existing coremltools inside the build environment
+pip uninstall -y coremltools
 
 # Create a directory for building the artifacts
 mkdir -p ${BUILD_DIR}
