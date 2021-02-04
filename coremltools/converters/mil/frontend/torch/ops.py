28b7af219cc67faf113e42056ab545e95ac3c67c
Mikael von Holst <vonholst@gmail.com>
2021-02-04 07:28:56 +0100

    add MIL ops for torch reflection pad

--- coremltools/converters/mil/frontend/torch/ops.py
+++ coremltools/converters/mil/frontend/torch/ops.py
@@ -2732,3 +2732,19 @@ def log10(context, node):
     x = inputs[0]
     log_x = mb.log(x=x)
     context.add(mb.mul(x=log_x, y=1/_np.log(10.0)), node.name)
+
+@register_torch_op
+def reflection_pad2d(context, node):
+    inputs = _get_inputs(context, node)
+    x = inputs[0]
+    torch_pad = inputs[1].val
+    pad = [torch_pad[2], torch_pad[3], torch_pad[0], torch_pad[1]]
+    context.add(mb.pad(x=x, pad=pad, mode='reflect'), node.name)
+
+@register_torch_op
+def reflection_pad1d(context, node):
+    inputs = _get_inputs(context, node)
+    x = inputs[0]
+    torch_pad = inputs[1].val
+    pad = [0, 0, torch_pad[0], torch_pad[1]]
+    context.add(mb.pad(x=x, pad=pad, mode='reflect'), node.name)
