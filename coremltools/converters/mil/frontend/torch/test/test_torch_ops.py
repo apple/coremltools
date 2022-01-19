858543b79c69a12251cc17dbdf9c9e6def249031
Sergey Polezhaev <piteryo7@gmail.com>
2022-01-19 15:10:09 +0300

    Fixed shape for updated test

--- coremltools/converters/mil/frontend/torch/test/test_torch_ops.py
+++ coremltools/converters/mil/frontend/torch/test/test_torch_ops.py
@@ -2362,7 +2362,7 @@ class TestSplit(TorchBaseTest):
 class TestUnbind(TorchBaseTest):
     @pytest.mark.parametrize(
         "backend, input_shape, dim",
-        itertools.product(backends, [(0), (3, 3, 4)], [0,1,2]),
+        itertools.product(backends, [(1), (3, 3, 4)], [0,1,2]),
     )
     def test_unbind(self, backend, input_shape, dim):
         model = ModuleWrapper(function=torch.unbind,
