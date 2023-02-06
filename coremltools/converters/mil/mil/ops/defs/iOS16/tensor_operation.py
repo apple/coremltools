13b69cc9ae05bce830a922b2dbb66843635aa72b
Tony Bove <70229264+tonybove-apple@users.noreply.github.com>
2023-02-06 15:55:48 -0800

    Docs - Update v6.2 API Reference (#1756)
    Co-authored-by: above3 <anthony_bove@apple.com>

--- coremltools/converters/mil/mil/ops/defs/iOS16/tensor_operation.py
+++ coremltools/converters/mil/mil/ops/defs/iOS16/tensor_operation.py
@@ -66,14 +66,11 @@ class fill_like(Operation):
 @register_op(opset_version=_IOS16_TARGET)
 class topk(_topk_iOS15):
     """
-    A version of ``topk`` for iOS 16+. This section documents the differences. For the
+    A version of ``topk`` for iOS 16+. This section documents the differences. The following are additional parameters for the iOS 16+ version. For the
     rest of the documentation, see `the iOS 15 version of topk <#coremltools.converters.mil.mil.ops.defs.iOS15.tensor_operation.topk>`_.
 
     Parameters
     ----------
-    The following are additional parameters for the iOS 16+ version. (For more parameters,
-    see `the iOS 15 version of topk <#coremltools.converters.mil.mil.ops.defs.iOS15.tensor_operation.topk>`_.)
-    
     sort: const<bool> (Optional)
         * Defaults to ``True``.
         * If ``True``, ``top-k`` elements are themselves sorted.
