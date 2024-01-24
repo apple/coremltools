2c37aab095b1d596d8a359ea9bc051b8a31c0951
yifan_shen3 <yifan_shen3@apple.com>
2024-01-24 00:01:21 -0800

    polish comments to doc string

--- coremltools/converters/mil/frontend/torch/utils.py
+++ coremltools/converters/mil/frontend/torch/utils.py
@@ -14,25 +14,32 @@ class TorchFrontend(Enum):
 
 
 def sanitize_op_kind(op_kind: str) -> str:
-    # We conventionally skip the aten/prim namespaces in our naming
+    """
+    In our torch converter, we register torch ops only by its "canonical" name:
+    1. No namespace prefix if it is the common aten/prim: e.g. ``aten::softmax`` -> ``softmax``
+    2. No double underscore prefix and suffix: e.g. ``__add__`` -> ``add``
+    3. Lower-case characters only: e.g. ``mul.Tensor`` -> ``mul.tensor``
+    """
+    # 1. Skip the aten/prim namespace prefix
     namespace = op_kind.split("::")[0].lower()
     if namespace in _DEFAULT_OP_NAMESPACES:
         op_kind = op_kind.split("::")[-1]
 
-    # Some ops may have double underscore, e.g. `__and__`
-    # We conventionally remove such prefix and suffix, e.g. `__add__` -> `add`
+    # 2. Remove underscore prefix and suffix
     if op_kind.startswith("__") and op_kind.endswith("__"):
         op_kind = op_kind[2:-2]
 
-    # We conventionally use only lower-case characters
+    # 3. Lower case
     return op_kind.lower()
 
 
-# In many cases, CoreML uses only functional ops,
-# so we do not have to distinguish in-place from functional,
-# so we will want to remove the conventional in-place suffix `_` of PyTorch.
-# For instance, `sub_` -> `sub`
 def unify_inplace_and_functional(op_kind: str) -> str:
+    """
+    In many cases, CoreML uses only functional ops,
+    so we do not have to distinguish in-place from functional,
+    so we will want to remove the conventional in-place suffix ``_`` of PyTorch.
+    For instance, ``sub_`` -> ``sub``
+    """
     if op_kind.endswith("_"):
         op_kind = op_kind[:-1]
 
