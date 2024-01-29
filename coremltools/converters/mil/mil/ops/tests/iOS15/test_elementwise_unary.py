81c0b722da0f9f58729b4326fbec6d28f6c61f1d
Chin-Chang Yang <2770271+ChinChangYang@users.noreply.github.com>
2024-01-29 19:32:42 +0800

    Parametrize input and add expected output
    Modify test_cast to use parametrize and handle various input types,
    improving test coverage. This change helps ensure
    comprehensive testing for different data types.

--- coremltools/converters/mil/mil/ops/tests/iOS15/test_elementwise_unary.py
+++ coremltools/converters/mil/mil/ops/tests/iOS15/test_elementwise_unary.py
@@ -3,7 +3,9 @@
 #  Use of this source code is governed by a BSD-3-clause license that can be
 #  found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 
+import itertools
 import numpy as np
+import pytest
 from coremltools.converters.mil.mil.ops.defs.iOS15 import elementwise_unary
 
 
@@ -15,8 +17,24 @@ class MockInputVar:
 
 
 class TestCast:
-    def test_cast_float_without_tensor(self):
-        input_var = MockInputVar(val=1.0, sym_type=None)
-        output = elementwise_unary.cast.get_cast_value(input_var, "fp32")
-        expected_output = np.array(1.0, dtype=np.float32)
+    NUMPY_DTYPE_TO_STRING = {
+        np.int32: "int32",
+        np.float16: "fp16",
+        np.float32: "fp32",
+        np.bool_: "bool",
+    }
+
+    @pytest.mark.parametrize(
+        "value, dtype",
+        itertools.product(
+            [2.0, (0.0, 1.0)],
+            [np.int32, np.float16, np.float32, np.bool_],
+        ),
+    )
+    def test_cast(self, value, dtype):
+        input_var = MockInputVar(val=value, sym_type=None)
+        output = elementwise_unary.cast.get_cast_value(
+            input_var, self.NUMPY_DTYPE_TO_STRING[dtype]
+        )
+        expected_output = dtype(value)
         np.testing.assert_array_equal(output, expected_output)
