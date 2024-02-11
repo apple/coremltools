21cba735cea3036f7b0af5211485113216120797
Anurag Kumar <mailanu98@gmail.com>
2024-02-11 23:05:06 +0530

    Update setup.py
    python version updated

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development",
     ],
