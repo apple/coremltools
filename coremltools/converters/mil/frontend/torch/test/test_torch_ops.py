fix CI: should not overflow the dtype we are using

coremltools/converters/mil/frontend/torch/test/test_torch_ops.py
@@ -6148,7 +6148,7 @@ class TestTo(TorchBaseTest):
 
         inputs = [TensorType(name="input_data", shape=(1, 2, 3), dtype=np.int32)]
         self.run_compare_torch(
-            inputs, TestModel(), backend=backend, compute_unit=compute_unit
+            inputs, TestModel(), rand_range=(0, 127), backend=backend, compute_unit=compute_unit
         )
 
     @pytest.mark.parametrize(

coremltools/converters/mil/frontend/torch/test/testing_utils.py
@@ -3,6 +3,8 @@
 #  Use of this source code is governed by a BSD-3-clause license that can be
 #  found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
 
+from typing import List, Union
+
 import numpy as np
 import pytest
 import torch
@@ -125,7 +127,7 @@ def convert_to_mlmodel(
 
 def generate_input_data(
     input_size, rand_range=(0, 1), dtype=np.float32, torch_device=torch.device("cpu")
-) -> torch.Tensor:
+) -> Union[torch.Tensor, List[torch.Tensor]]:
     r1, r2 = rand_range
 
     def random_data(spec, dtype=np.float32):