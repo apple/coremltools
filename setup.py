0e292a072452db19d1e64b687a372c0c54704a90
dpanshu <deepanshu.iitk@gmail.com>
2024-06-25 17:29:16 -0700

    numpy 2.0.0 breaking changes (#2255)

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
         ]
     },
     install_requires=[
-        "numpy >= 1.14.5",
+        "numpy >= 1.14.5, < 2.0.0",
         "protobuf >= 3.1.0, <= 4.0.0",
         "sympy",
         "tqdm",
