b1240e79625edbc8dc835679bd348ba59e04ef12
RGooBS24 <101910151+RGooBS24@users.noreply.github.com>
2025-01-23 17:14:10 -0700

    replaced mktemp with NamedTemporaryFile for issue 2430 Fix Insecure Temporary File (#2437)

--- coremltools/test/sklearn_tests/test_NuSVC.py
+++ coremltools/test/sklearn_tests/test_NuSVC.py
@@ -297,7 +297,7 @@ class NuSVCLibSVMTest(unittest.TestCase):
                 self.assertEqual(metrics["num_errors"], 0)
 
     def test_conversion_from_filesystem(self):
-        libsvm_model_path = tempfile.mktemp(suffix="model.libsvm")
+        libsvm_model_path = tempfile.NamedTemporaryFile(suffix="model.libsvm").name
         svmutil.svm_save_model(libsvm_model_path, self.libsvm_model)
         spec = libsvm.convert(libsvm_model_path, "data", "target")
 
