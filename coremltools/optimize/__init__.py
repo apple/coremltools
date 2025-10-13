a5558a66eff8ec68b7209b1a64fef133fb3dd918
Ehnnethgarcia13 <clarenzcerennethgarcia@gmail.com>
2025-10-13 08:09:23 +0800

    Delete coremltools directory

--- coremltools/optimize/__init__.py
+++
@@ -1,9 +0,0 @@
-#  Copyright (c) 2023, Apple Inc. All rights reserved.
-#
-#  Use of this source code is governed by a BSD-3-clause license that can be
-#  found in the LICENSE.txt file or at https://opensource.org/licenses/BSD-3-Clause
-
-from coremltools._deps import _IMPORT_CT_OPTIMIZE_TORCH
-
-if _IMPORT_CT_OPTIMIZE_TORCH:
-    from . import torch
