c68b00e9b14c58e60daf7c27c5ef06bd5599958a
gokulkrishna98 <gokulkrishna98@gmail.com>
2025-11-03 13:48:00 -0800

    9.0 Release

--- setup.py
+++ setup.py
@@ -106,6 +106,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development",
     ],
